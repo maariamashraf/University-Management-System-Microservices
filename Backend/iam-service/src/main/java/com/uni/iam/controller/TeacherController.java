9d3b723aa0a4ad0a67ddde6223bfd51fb0ae4104
mahmoud-1 <mahmood444e@gmail.com>
2026-05-05 01:34:35 +0300

    solve the iam issue

--- Backend/iam-service/src/main/java/com/uni/iam/controller/TeacherController.java
+++ Backend/iam-service/src/main/java/com/uni/iam/controller/TeacherController.java
@@ -1,5 +1,4 @@
 package com.uni.iam.controller;
-
 import com.uni.iam.dto.response.TeacherResponse;
 import com.uni.iam.dto.response.TeacherProfileResponse;
 import com.uni.iam.service.interfaces.TeacherService;
@@ -8,8 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
